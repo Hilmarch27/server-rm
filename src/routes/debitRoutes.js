f24e94e996b0166b1dcf9ea9fda5013fa232c363
Hilmarch27 <hilmarch03@gmail.com>
2024-07-11 06:11:16 +0700

    new update

--- src/routes/debitRoutes.js
+++ src/routes/debitRoutes.js
@@ -1,76 +1,33 @@
 // src/routes/authRoutes.js
 
 import express from "express";
-import {
-  getDebitursByLoggedInUser,
-  getDebitursByNama,
-  addMultipleDebitur,
-} from "../controllers/debitursController.js";
+import { addMultipleDebiturController, createActController, getActByDebiturIdController, getDebitursByLoggedInUserController, getDebitursByNamaController, updateDebitur } from "../controllers/debiturController.js";
 import { authenticateUser } from "../middlewares/authMiddleware.js";
-import {
-  addPemberkasan,
-  addPenagihan,
-  getActivity,
-  updatePenagihan,
-  updatePemberkasan,
-} from "../controllers/activityController.js";
-import multer from "multer";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Navigasi satu tingkat di atas dan menuju ke direktori uploads
-const uploadDir = path.join(__dirname, "../uploads");
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
-
-const upload = multer({ storage });
+import {uploadFields} from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.get("/debiturs", authenticateUser, getDebitursByLoggedInUser);
-router.get("/debiturs/:nama", getDebitursByNama);
-router.post("/multiple-debiturs", addMultipleDebitur);
-router.post("/add-penagihan", authenticateUser, addPenagihan);
+const fields = [
+  { name: "fotoAgunan1", maxCount: 1 },
+  { name: "fotoAgunan2", maxCount: 1 },
+  { name: "fotoAgunan3", maxCount: 1 },
+  { name: "fotoAgunan4", maxCount: 1 },
+  { name: "fotoAgunan5", maxCount: 1 },
+  { name: "fotoAgunan6", maxCount: 1 },
+];
+
+router.get("/debiturs", authenticateUser, getDebitursByLoggedInUserController);
+router.get("/debiturs/:nama", getDebitursByNamaController);
+router.post("/multiple-debiturs", addMultipleDebiturController);
+router.put("/update-debitur/:id", updateDebitur);
+
+// activity
 router.post(
-  "/add-pemberkasan",
+  "/add-act",
   authenticateUser,
-  upload.fields([
-    { name: "foto_depan", maxCount: 1 },
-    { name: "foto_kiri", maxCount: 1 },
-    { name: "foto_kanan", maxCount: 1 },
-    { name: "foto_dalam_rumah1", maxCount: 1 },
-    { name: "foto_dalam_rumah2", maxCount: 1 },
-    { name: "foto_dalam_rumah3", maxCount: 1 },
-  ]),
-  addPemberkasan
+  uploadFields(fields),
+  createActController
 );
-
-// Route untuk update data
-router.put("/update-penagihan/:id", authenticateUser, updatePenagihan);
-router.put(
-  "/update-pemberkasan/:id",
-  authenticateUser,
-  upload.fields([
-    { name: "foto_depan", maxCount: 1 },
-    { name: "foto_kiri", maxCount: 1 },
-    { name: "foto_kanan", maxCount: 1 },
-    { name: "foto_dalam_rumah1", maxCount: 1 },
-    { name: "foto_dalam_rumah2", maxCount: 1 },
-    { name: "foto_dalam_rumah3", maxCount: 1 },
-  ]),
-  updatePemberkasan
-);
-
-router.get("/activity/:nama", getActivity);
+router.get("/get-act", getActByDebiturIdController);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -5,14 +5,12 @@ import authRoutes from "./routes/authRoutes.js";
 import debitRoutes from "./routes/debitRoutes.js";
 import cors from "cors";
 import chalk from "chalk";
-import { PrismaClient } from "@prisma/client";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const prisma = new PrismaClient();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
